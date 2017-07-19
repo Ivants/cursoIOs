4e50255a4c7a12bf3cc22ca9ff03fa72ce964aad
Admin <admin@iMac-de-Carlos.local>
2017-07-18 23:08:51 -0500

    Modificaciones para el boton de enter. Aparicion y desaparicion del teclado desde codigo. Asociacion del delegate con entorno grafico y con codigo

--- MyPrimeraApp/ViewController.swift
+++ MyPrimeraApp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     //Con esta variable vamos a hacer la conexion entre la parte visual y el codigo
     //Tambien puedo crearlo con ctl y jalandolo hasta el codigo
     
@@ -29,6 +29,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //mLabel.text = "Este es un nuevo mensaje"
+        
+        
+        //De esta forma se asocia un elemento con el delegate
+        //mtextField.delegate = self
     }
     
     //Se manda a llamar de acuerdo al toque (Gestos)
@@ -36,6 +40,7 @@ class ViewController: UIViewController {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
+    
     
     @IBAction func onClick(_ sender: Any) {
         let texto = mtextField.text!
@@ -49,6 +54,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //Nosotros tenemos que indicarle a la aplicacion que queremos que el boton de enter del teclado tenga una funcionalidad en este caso al dar click en ese boton reasignamos el fisrtResponder para que el teclado se oculte despues de dar un enter
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        mtextField.resignFirstResponder()
+        return false
     }
 
 }
